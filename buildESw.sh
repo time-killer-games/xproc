ddc200ba660b2769fa61a00622577086b2211aa4
time-killer-games <samuelvenable@hotmail.com>
2021-06-02 16:42:16 -0400

    Update buildESw.sh

--- buildESw.sh
+++ buildESw.sh
@@ -2,7 +2,7 @@
 cd "${0%/*}"
 
 if [ $(uname) = "Linux" ]; then
-  g++ main.cpp crossprocess.cpp -o xproc -std=c++17 -static-libgcc -static-libstdc++ -lprocps -lpthread -DXPROCESS_GUIWINDOW_IMPL `shell pkg-config x11 --cflags --libs`;
+  g++ main.cpp crossprocess.cpp -o xproc -std=c++17 -static-libgcc -static-libstdc++ -lprocps -lpthread -DXPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs`;
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ main.cpp crossprocess.cpp -o xproc -std=c++17 -lprocstat -lutil -lc -lpthread -DXPROCESS_GUIWINDOW_IMPL `shell pkg-config x11 --cflags --libs`;
+  clang++ main.cpp crossprocess.cpp -o xproc -std=c++17 -lprocstat -lutil -lc -lpthread -DXPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs`;
 fi
