c933a321e535f5294b223cd459adb5beb4cc1880
time-killer-games <samuelvenable@hotmail.com>
2021-06-22 09:41:32 -0400

    Add files via upload

--- build32xw.sh
+++ build32xw.sh
@@ -14,4 +14,5 @@ else
   xxd -i 'crossprocess64' | sed 's/\([0-9a-f]\)$/\0, 0x00/' > 'crossprocess64.h'
   rm -f "crossprocess32.exe" "crossprocess64.exe"
   C:/msys64/msys2_shell.cmd -defterm -mingw32 -no-start -here -lc "g++ main.cpp crossprocess.cpp -o xproc.exe -std=c++17 -static-libgcc -static-libstdc++ -static -lShlwapi -DXPROCESS_WIN32EXE_INCLUDES -DXPROCESS_GUIWINDOW_IMPL -m32";
+  rm -f "crossprocess32.h" "crossprocess64.h"
 fi
