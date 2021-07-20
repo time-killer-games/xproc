ad15794b10c74a2e1287a481f11971e2b14bb5ca
time-killer-games <samuelvenable@hotmail.com>
2021-07-20 06:22:32 -0400

    Add files via upload

--- crossprocess.cpp
+++ crossprocess.cpp
@@ -707,7 +707,7 @@ void CwdFromProcId(PROCID procId, char **buffer) {
     }
     PROCESS ind = ProcessExecute(("\"" + exe + "\" --cwd-from-pid " + std::to_string(procId)).c_str());
     static std::string str; if (stdOptMap.find(ind) != stdOptMap.end()) str = stdOptMap.find(ind)->second;
-    if (!str.empty() && str.back() == "\\") {
+    if (!str.empty() && str.back() == '\\') {
       *buffer = (char *)str.substr(0, str.length() - 1).c_str();
     } else {
       *buffer = (char *)str.c_str();
@@ -719,7 +719,7 @@ void CwdFromProcId(PROCID procId, char **buffer) {
     CwdCmdEnvFromProc(proc, &cwdbuf, MEMCWD);
     if (cwdbuf) {
       static std::string str; str = narrow(cwdbuf);
-      if (!str.empty() && str.back() == "\\") {
+      if (!str.empty() && str.back() == '\\') {
         *buffer = (char *)str.substr(0, str.length() - 1).c_str();
       } else {
         *buffer = (char *)str.c_str();
