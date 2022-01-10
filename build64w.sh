35dd6fd881f405fea2c844c5375893d478e67413
Samuel Venable <samuelvenable@hotmail.com>
2022-01-09 20:12:13 -0500

    Update build64w.sh

--- build64w.sh
+++ build64w.sh
@@ -2,19 +2,19 @@
 cd "${0%/*}"
 
 if [ $(uname) = "Darwin" ]; then
-  clang++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -DPROCESS_GUIWINDOW_IMPL -framework CoreFoundation -framework CoreGraphics -arch arm64 -arch x86_64;
+  clang++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -DPROCESS_GUIWINDOW_IMPL -framework Cocoa -framework CoreFoundation -framework CoreGraphics -arch arm64 -arch x86_64;
 elif [ $(uname) = "Linux" ]; then
-  g++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -static-libgcc -static-libstdc++ -lprocps -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -m64;
+  g++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -static-libgcc -static-libstdc++ -lprocps -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs`;
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -lprocstat -lutil -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -m64;
+  clang++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -lprocstat -lutil -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs`;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -lkvm -lutil -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -m64;
+  g++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -lkvm -lutil -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs`;
 else
   C:/msys64/msys2_shell.cmd -defterm -mingw32 -no-start -here -lc "g++ apiprocess/process.cpp -o process32.exe -std=c++17 -static-libgcc -static-libstdc++ -static -m32";
   C:/msys64/msys2_shell.cmd -defterm -mingw64 -no-start -here -lc "g++ apiprocess/process.cpp -o process64.exe -std=c++17 -static-libgcc -static-libstdc++ -static -m64";
   xxd -i 'process32' | sed 's/\([0-9a-f]\)$/\0, 0x00/' > 'apiprocess/process32.h'
   xxd -i 'process64' | sed 's/\([0-9a-f]\)$/\0, 0x00/' > 'apiprocess/process64.h'
   rm -f "process32.exe" "process64.exe"
-  C:/msys64/msys2_shell.cmd -defterm -mingw64 -no-start -here -lc "g++ main.cpp apiprocess/process.cpp -o xproc.exe -std=c++17 -static-libgcc -static-libstdc++ -static -DPROCESS_WIN32EXE_INCLUDES -DPROCESS_GUIWINDOW_IMPL -m64";
+  C:/msys64/msys2_shell.cmd -defterm -mingw64 -no-start -here -lc "g++ main.cpp apiprocess/process.cpp -o xproc.exe -std=c++17 -static-libgcc -static-libstdc++ -static -DPROCESS_WIN32EXE_INCLUDES -DPROCESS_GUIWINDOW_IMPL";
   rm -f "apiprocess/process32.h" "apiprocess/process64.h"
 fi
