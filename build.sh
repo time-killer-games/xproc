077c86bfd9b360ad3a707891a727907e4252a5c9
Samuel Venable <samuelvenable@hotmail.com>
2022-05-07 06:51:34 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ elif [ $(uname) = "FreeBSD" ]; then
   clang++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -lprocstat -lutil -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs`;
 elif [ $(uname) = "DragonFly" ]; then
   g++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -static-libgcc -static-libstdc++ -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs`;
+elif [ $(uname) = "NetBSD" ]; then
+  g++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -static-libgcc -static-libstdc++ -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs`;
 elif [ $(uname) = "OpenBSD" ]; then
   clang++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs`;
 else
