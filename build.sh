6456ee4e0417560a7bf1e8a26831847453e27526
Samuel Venable <samuelvenable@hotmail.com>
2022-08-07 12:52:18 -0400

    Add files via upload

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 cd "${0%/*}"
 
 if [ $(uname) = "Darwin" ]; then
-  clang++ main.cpp apiprocess/xproc.cpp -o xproc -std=c++17 -arch arm64 -arch x86_64;
+  clang++ main.cpp apiprocess/xproc.cpp -o xproc -std=c++17 -Wall -arch arm64 -arch x86_64;
 elif [ $(uname) = "Linux" ]; then
-  g++ main.cpp apiprocess/xproc.cpp -o xproc -std=c++17 -static-libgcc -static-libstdc++ -lprocps;
+  g++ main.cpp apiprocess/xproc.cpp -o xproc -std=c++17 -Wall -static-libgcc -static-libstdc++ -lprocps;
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ main.cpp apiprocess/xproc.cpp /usr/lib/libkvm.a /usr/lib/libprocstat.a /usr/lib/libutil.a /usr/lib/libelf.a /usr/lib/libc.a -o xproc -std=c++17 -static;
+  clang++ main.cpp apiprocess/xproc.cpp /usr/lib/libkvm.a /usr/lib/libprocstat.a /usr/lib/libutil.a /usr/lib/libelf.a /usr/lib/libc.a -o xproc -std=c++17 -Wall -static;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ main.cpp apiprocess/xproc.cpp /usr/lib/libkvm.a /usr/lib/libc.a -o xproc -std=c++17 -static-libgcc -static-libstdc++ -static;
+  g++ main.cpp apiprocess/xproc.cpp /usr/lib/libkvm.a /usr/lib/libc.a -o xproc -std=c++17 -Wall -static-libgcc -static-libstdc++ -static;
 elif [ $(uname) = "NetBSD" ]; then
-  g++ main.cpp apiprocess/xproc.cpp /usr/lib/libkvm.a /usr/lib/libc.a -o xproc -std=c++17 -static-libgcc -static-libstdc++ -static;
+  g++ main.cpp apiprocess/xproc.cpp /usr/lib/libkvm.a /usr/lib/libc.a -o xproc -std=c++17 -Wall -static-libgcc -static-libstdc++ -static;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ main.cpp apiprocess/xproc.cpp /usr/lib/libkvm.a /usr/lib/libc.a -o xproc -std=c++17 -static;
+  clang++ main.cpp apiprocess/xproc.cpp /usr/lib/libkvm.a /usr/lib/libc.a -o xproc -std=c++17 -Wall -static;
 else
-  g++ main.cpp apiprocess/xproc.cpp -o xproc.exe -std=c++17 -static-libgcc -static-libstdc++ -static;
+  g++ main.cpp apiprocess/xproc.cpp -o xproc.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static;
 fi
