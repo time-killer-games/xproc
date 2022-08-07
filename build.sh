543655ef29125c13f9c7655e2827f5cd57ef7b42
Samuel Venable <samuelvenable@hotmail.com>
2022-08-07 09:47:37 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 cd "${0%/*}"
 
 if [ $(uname) = "Darwin" ]; then
-  clang++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -arch arm64 -arch x86_64;
+  clang++ main.cpp apiprocess/xproc.cpp -o xproc -std=c++17 -arch arm64 -arch x86_64;
 elif [ $(uname) = "Linux" ]; then
-  g++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -static-libgcc -static-libstdc++ -lprocps;
+  g++ main.cpp apiprocess/xproc.cpp -o xproc -std=c++17 -static-libgcc -static-libstdc++ -lprocps;
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -lprocstat -lutil -lc;
+  clang++ main.cpp apiprocess/xproc.cpp -o xproc -std=c++17 -lprocstat -lutil -lc;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -static-libgcc -static-libstdc++ -lkvm -lc;
+  g++ main.cpp apiprocess/xproc.cpp -o xproc -std=c++17 -static-libgcc -static-libstdc++ -lkvm -lc;
 elif [ $(uname) = "NetBSD" ]; then
-  g++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -static-libgcc -static-libstdc++ -lkvm -lc;
+  g++ main.cpp apiprocess/xproc.cpp -o xproc -std=c++17 -static-libgcc -static-libstdc++ -lkvm -lc;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ main.cpp apiprocess/process.cpp -o xproc -std=c++17 -lkvm -lc;
+  clang++ main.cpp apiprocess/xproc.cpp -o xproc -std=c++17 -lkvm -lc;
 else
-  g++ main.cpp apiprocess/process.cpp -o xproc.exe -std=c++17 -static-libgcc -static-libstdc++ -static;
+  g++ main.cpp apiprocess/xproc.cpp -o xproc.exe -std=c++17 -static-libgcc -static-libstdc++ -static;
 fi
