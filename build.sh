c4711aeb4ab9d6a3de3d2c40631364c06a1bbb43
Samuel Venable <samuelvenable@hotmail.com>
2022-08-10 08:48:29 -0400

    Add files via upload

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd "${0%/*}"
 if [ $(uname) = "Darwin" ]; then
   clang++ main.cpp apiprocess/xproc.cpp -o xproc -std=c++17 -Wall -arch arm64 -arch x86_64;
 elif [ $(uname) = "Linux" ]; then
-  g++ main.cpp apiprocess/xproc.cpp -o xproc -std=c++17 -Wall -static-libgcc -static-libstdc++ -lprocps;
+  g++ main.cpp apiprocess/xproc.cpp -o xproc -std=c++17 -Wall -static-libgcc -static-libstdc++ -static;
 elif [ $(uname) = "FreeBSD" ]; then
   clang++ main.cpp apiprocess/xproc.cpp /usr/lib/libkvm.a /usr/lib/libprocstat.a /usr/lib/libutil.a /usr/lib/libelf.a /usr/lib/libc.a -o xproc -std=c++17 -Wall -static;
 elif [ $(uname) = "DragonFly" ]; then
