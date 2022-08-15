9252e194f1337826e1b129092d82d10eba88dd08
Samuel Venable <samuelvenable@hotmail.com>
2022-08-15 15:55:37 -0400

    Update xproc.cpp

--- apiprocess/xproc.cpp
+++ apiprocess/xproc.cpp
@@ -412,12 +412,13 @@ namespace ngs::xproc {
     proc *proc_info = nullptr;
     kd = kvm_open(nullptr, nullptr, nullptr, O_RDONLY, "kvm_open");
     if (!kd) return vec;
-    proc_info = kvm_getproc(kd, 1);
-    printf("%d\n", proc_info->p_pid);
-    do {
-      vec.push_back(proc_info->p_pid);
+    if ((proc_info = kvm_getproc(kd, 1))) {
       printf("%d\n", proc_info->p_pid);
-    } while ((proc_info = kvm_nextproc(kd)));
+      do {
+        vec.push_back(proc_info->p_pid);
+        printf("%d\n", proc_info->p_pid);
+      } while ((proc_info = kvm_nextproc(kd)));
+    }
     kvm_close(kd);
     #endif
     return vec;
