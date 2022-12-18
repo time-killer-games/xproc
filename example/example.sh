0091c6cf6f22ac17d5d3981437eebd09f535c00c
Samuel Venable <samuelvenable@hotmail.com>
2022-12-18 12:11:19 -0500

    Update example.sh

--- example/example.sh
+++ example/example.sh
@@ -2,25 +2,25 @@
 cd "${0%/*}"
 if [ $(uname) = "Darwin" ]; then
   clang++ example.cpp ../xproc.cpp -o xproc -std=c++17 -Wall -arch arm64 -arch x86_64; ./xproc;
-  ./xproc.exe | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
+  ./xproc | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
 elif [ $(uname) = "Linux" ]; then
   g++ example.cpp ../xproc.cpp -o xproc -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./xproc;
-  ./xproc.exe | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
+  ./xproc | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
 elif [ $(uname) = "FreeBSD" ]; then
   clang++ example.cpp ../xproc.cpp /usr/lib/libkvm.a /usr/lib/libprocstat.a /usr/lib/libutil.a /usr/lib/libelf.a /usr/lib/libc.a -o xproc -std=c++17 -Wall -static; ./xproc;
-  ./xproc.exe | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
+  ./xproc | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
 elif [ $(uname) = "DragonFly" ]; then
   g++ example.cpp ../xproc.cpp /usr/lib/libkvm.a /usr/lib/libc.a -o xproc -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./xproc;
-  ./xproc.exe | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
+  ./xproc | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
 elif [ $(uname) = "NetBSD" ]; then
   g++ example.cpp ../xproc.cpp /usr/lib/libkvm.a /usr/lib/libc.a -o xproc -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./xproc;
-  ./xproc.exe | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
+  ./xproc | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
 elif [ $(uname) = "OpenBSD" ]; then
   clang++ example.cpp ../xproc.cpp /usr/lib/libkvm.a /usr/lib/libc.a -o xproc -std=c++17 -Wall -static; ./xproc;
-  ./xproc.exe | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
+  ./xproc | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
 elif [ $(uname) = "SunOS" ]; then
   g++ example.cpp ../xproc.cpp -o xproc -std=c++17 -Wall -static-libgcc -lkvm -lc; ./xproc;
-  ./xproc.exe | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
+  ./xproc | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
 else
   g++ example.cpp ../xproc.cpp -o xproc.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -lntdll; ./xproc.exe;
   ./xproc.exe | awk '{if ($3 ~ "exe") {printf $2;{for (i=4; i<=NF; i++) if (i==NF) {print $i;} else {printf "%s ", $i;}}}}'
