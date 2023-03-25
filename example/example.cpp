a007326a508651488e358f0bcb27d7743568770d
Samuel Venable <samuelvenable@hotmail.com>
2023-03-24 20:26:39 -0500

    Add files via upload

--- example/example.cpp
+++ example/example.cpp
@@ -31,44 +31,30 @@
 #include "../xproc.hpp"
 
 int main(int argc, char **argv) {
-
-  std::vector<ngs::ps::PROCID> pid;
+  std::vector<ngs::ps::NGS_PROCID> pid;
   if (argc == 1) pid = ngs::ps::proc_id_enum();
-
   for (int i = 1; i < argc; i++) {
-
-    std::vector<ngs::ps::PROCID> exe = ngs::ps::proc_id_from_exe(argv[i]);
-    std::vector<ngs::ps::PROCID> cwd = ngs::ps::proc_id_from_cwd(argv[i]);
+    std::vector<ngs::ps::NGS_PROCID> exe = ngs::ps::proc_id_from_exe(argv[i]);
+    std::vector<ngs::ps::NGS_PROCID> cwd = ngs::ps::proc_id_from_cwd(argv[i]);
     pid.insert(pid.end(), exe.begin(), exe.end());
     pid.insert(pid.end(), cwd.begin(), cwd.end());
-
   }
-
   for (std::size_t i = 0; i < pid.size(); i++) {
-
     std::string exe = ngs::ps::exe_from_proc_id(pid[i]);
     if (!exe.empty()) std::cout << "pid[" << i << "]: " << pid[i] << ", exe: " << exe << "\n";
-
     std::string cwd = ngs::ps::cwd_from_proc_id(pid[i]);
     if (!cwd.empty()) std::cout << "pid[" << i << "]: " << pid[i] << ", cwd: " << cwd << "\n";
-  
-    std::vector<ngs::ps::PROCID> ppid = ngs::ps::parent_proc_id_from_proc_id(pid[i]);
+    std::vector<ngs::ps::NGS_PROCID> ppid = ngs::ps::parent_proc_id_from_proc_id(pid[i]);
     if (!ppid.empty()) std::cout << "pid[" << i << "]: " << pid[i] << ", ppid: " << ppid[0] << "\n";
-
-    std::vector<ngs::ps::PROCID> cpid = ngs::ps::proc_id_from_parent_proc_id(pid[i]);
+    std::vector<ngs::ps::NGS_PROCID> cpid = ngs::ps::proc_id_from_parent_proc_id(pid[i]);
     for (std::size_t j = 0; j < cpid.size(); j++)
       std::cout << "pid[" << i << "]: " << pid[i] << ", cpid[" << j << "]: " << cpid[j] << "\n";
-
     std::vector<std::string> cmd = ngs::ps::cmdline_from_proc_id(pid[i]);
     for (std::size_t j = 0; j < cmd.size(); j++)
       std::cout << "pid[" << i << "]: " << pid[i] << ", cmd[" << j << "]: " << cmd[j] << "\n";
-
     std::vector<std::string> env = ngs::ps::environ_from_proc_id(pid[i]);
     for (std::size_t j = 0; j < env.size(); j++)
       std::cout << "pid[" << i << "]: " << pid[i] << ", env[" << j << "]: " << env[j] << "\n";
-
   }
-
   return 0;
-
 }
